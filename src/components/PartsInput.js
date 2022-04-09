feb4c05425938d339e968180996c276a53a49bd4
jacpolanski <jac.polanski@gmail.com>
2022-04-09 19:28:05 +0200

    Minor style adjustments

--- src/components/PartsInput.js
+++ src/components/PartsInput.js
@@ -6,24 +6,30 @@ function PartsInput() {
     <Form className="p-3 mb-5">
       <div className="mb-3 d-lg-flex">
         <div className="d-flex flex-column justify-content-between w-100">
-          <Form.Group className="w-100" controlId="formPartName">
+          <Form.Group className="w-100 mb-3 mb-lg-0" controlId="formPartName">
             <Form.Label>Part Name</Form.Label>
             <Form.Control placeholder="Please enter part name" required />
           </Form.Group>
 
-          <Form.Group className="w-100 d-none" controlId="formNewCategory">
+          <Form.Group
+            className="w-100 mb-3 mb-lg-0 d-none"
+            controlId="formNewCategory"
+          >
             <Form.Label>Add NEW Part Category</Form.Label>
             <Form.Control placeholder="Enter new part category" />
           </Form.Group>
 
-          <Form.Group className="w-100" controlId="formPartCategory">
+          <Form.Group
+            className="w-100 mb-3 mb-lg-0"
+            controlId="formPartCategory"
+          >
             <Form.Label>Add Part Category</Form.Label>
             <Form.Select>
               <option>Default select</option>
             </Form.Select>
           </Form.Group>
 
-          <Form.Group className="w-100" controlId="formPartPrice">
+          <Form.Group className="w-100 mb-3 mb-lg-0" controlId="formPartPrice">
             <Form.Label>Add Part Price</Form.Label>
             <Form.Control placeholder="Enter part price" />
           </Form.Group>
