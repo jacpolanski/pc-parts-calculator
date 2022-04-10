f4d3ef254c7a3a94f832046625ce9bd8ad48ed7a
jacpolanski <jac.polanski@gmail.com>
2022-04-10 16:16:01 +0200

    Minor styling adjustments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,17 +5,20 @@ import logo from "../assets/logo.svg";
 function NavBar() {
   return (
     <Navbar bg="white" expand="lg">
-      <Container className="px-4">
+      <Container className="px-4 justify-content-sm-between justify-content-center">
         <Navbar.Brand href="/">
           <Image src={logo} style={{ height: 100 }} />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto justify-content-end w-100 fs-4">
-            <a href="/" className="nav-link px-3">
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="mt-4 bg-light"
+        />
+        <Navbar.Collapse id="basic-navbar-nav" className="text-center">
+          <Nav className="my-5 me-auto w-100 fs-4 ">
+            <a href="/" className="nav-link p-3">
               Main
             </a>
-            <a href="/about" className="nav-link px-3">
+            <a href="/about" className="nav-link p-3">
               About me
             </a>
           </Nav>
